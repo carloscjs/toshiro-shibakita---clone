06ebc2747a21ffb422440f55b0aa1de897b5ceff
Carlos José da Silva <103138801+carloscjs@users.noreply.github.com>
2022-12-15 21:05:41 -0300

    Update banco.sql

--- banco.sql
+++ banco.sql
@@ -1,8 +1,9 @@
-CREATE TABLE dados (
+CREATE TABLE Alunos (
     AlunoID int,
     Nome varchar(50),
     Sobrenome varchar(50),
     Endereco varchar(150),
     Cidade varchar(50),
-    Idade varchar(50)
+    Idade varchar(50),
+    Serie varchar(10)
 );
